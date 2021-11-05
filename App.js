c6029e7e5cdfd0d40487dc605ad60eec92179fe4
Muhammadsheraz492 <93596269+MuhammadSheraz4119@users.noreply.github.com>
2021-11-05 13:19:54 -0700

    'First Run Show Log after reload then Does not Show'

--- App.js
+++ App.js
@@ -4,39 +4,39 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import onbording from "./Screens/onbording";
 import Login from "./Screens/Login";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AppStack = createNativeStackNavigator();
 
 export default function App() {
   const [IsFirstScreenLogin, setIsFirstScreenLogin] = React.useState(null);
-  // React.useEffect(() => {
-  //   AsyncStorage.getItem("alreadyLaunched").then((Value) => {
-  //     if (Value == null) {
-  //       AsyncStorage.setItem("alreadyLaunched", "true");
-  //       setIsFirstScreenLogin(true);
-  //     }
-  //   });
-  // }, []);
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator>
-        <AppStack.Screen
-          name="onbording"
-          component={onbording}
-          options={{ headerShown: false }}
-        />
-        <AppStack.Screen name="Login" component={Login} />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
-}
+  React.useEffect(() => {
+    AsyncStorage.getItem("alreadyLaunched").then((Value) => {
+      if (Value == null) {
+        AsyncStorage.setItem("alreadyLaunched", "true");
+        setIsFirstScreenLogin(true);
+      } else {
+        setIsFirstScreenLogin(false);
+      }
+    });
+  }, []);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  if (IsFirstScreenLogin == null) {
+    return null;
+  } else if (IsFirstScreenLogin == true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator>
+          <AppStack.Screen
+            name="onbording"
+            component={onbording}
+            options={{ headerShown: false }}
+          />
+          <AppStack.Screen name="Login" component={Login} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return <Login />;
+  }
+}
