55d777310cc5c38e38b629a1b9db18d2581f1a83
hassam <hassamsohail009@gmail.com>
2022-01-02 22:26:37 -0800

    no

--- Screens/onbording.js
+++ Screens/onbording.js
@@ -38,7 +38,7 @@ const onbording = ({ navigation }) => {
       <Text style={{ fontSize: 16 }}>Done</Text>
     </TouchableOpacity>
   );
-  //hhwhwe
+
   return (
     <Onboarding
       SkipButtonComponent={Skip}
